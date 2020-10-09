e946dbbeeac1ae0241cabbd857248906480ff02a
facu diaz <facu.diaz777@gmail.com>
2020-10-08 21:32:05 -0300

    clase 2 intermedio

--- clase_2/src/App.js
+++ clase_2/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Counter from './Counter';
 
 const App = () => {
   //JSX
   //return React.createElement('div', {className:"unaClase"}, React.createElement('div,{}, "kajshdkajsdh"));
 
-  return (
-    <div className="">
-      <div>kajshdkajsdh</div>
-    </div>
-  );
+  return <Counter></Counter>;
 };
 
 render(React.createElement(App), document.getElementById('root'));
